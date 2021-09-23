13b1e860ad3f4fe043d2239d4ef42e42f5c5ef38
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2021-09-23 19:01:46 -0400

    updates

--- rename.php
+++ rename.php
@@ -1,41 +1,4 @@
 <?php
-$a = array("[ Johann Jungmann ] [ Anna Jungfleisch ]",
-"[ Johann Bartold Beerman ] [ Unknown ]",
-"[ Johann Paul Zollinger ] [ Johannette Margarethe Unknown_Surname ]",
-"[ Gottlieb Krückeberg ] [ Unknown ]",
-"[ Friedrich Wilhelm Weiland ] [ Unknown ]",
-"[ Johann Dietrich Kleinschmidt ] [ Marg ]",
-"[ Johannes Vollmer ] [ Wildburga Ensinger ]",
-"[ Friederich Maier ] [ Maria Agnes Bossler ]",
-"[ Unknown_firstname Feld ] [ Elisabeth Unknown ]",
-"[ Jacob Feld ] [ Catherine Elisabeth Jungmann ]",
-"[ Johann Henrich Beerman ] [ Caroline Stuber ]",
-"[ Ernest Geble ] [ Eleonora Geberelia ]",
-"[ Johann \"Christian\" Zollinger ] [ Dorothee Catharine Elisabethe Kühn ]",
-"[ Jacob Heinrich Kohlinger ] [ Mary Christina Weber ]",
-"[ Carl Friedrich Gottlieb Krückeberg ] [ Philippine Leonore Bleeken ]",
-"[ Johann Heinrich Christian Kleinschmidt ] [ Clare Christine Elisabeth Brandhorst ]",
-"[ Johann Georg Vollmer ] [ Johanna Dorothea Attinger ]",
-"[ Gottlieb Maier ] [ Maria Agnes Wieland ]",
-"[ Friedrich Heine ] [ Marie Sophie Franke ]",
-"[ Johann Jacob Peppler ] [ Maria Magdalene Platt ]",
-"[ Johannes Bender ] [ Katharine Elisabeth Rühl ]",
-"[ Friedrich Albersmeier ] [ Hanne Marie Sophie Louise Heine ]",
-"[ Ludwig \"Louis\" Peppler ] [ Mary Bender ]",
-"[ Peter Phillip Felt ] [ Margaret K ]",
-"[ Heinrich \"henry\" Behrman ] [ Justine Justina L Grieble ]",
-"[ Frederick Wilhelm Georg Zollinger ] [ Catharine Elizabeth Koehlinger ]",
-"[ Carl Friedrich Krückeberg ] [ Luise Dorothea Weiland ]",
-"[ Louis H Peppler ] [ Catherine Felt ]",
-"[ Louis L Behrman ] [ Wilhelmina \"Minnie\" Zollinger ]",
-"[ Carl Heinrich Wilhelm Krückeberg ] [ Caroline Wilhelmine Marie Kleinschmidt ]",
-"[ Karl Henry Ludwig (Lewis) Koldewey ] [ Sophie Margarite \"Louise\" Tiemann ]",
-"[ Johann Christian Vollmer ] [ Rosina Katharina \"katherine\" Maier ]",
-"[ Friedrich Christian Wilhelm Krueckeberg ] [ Emma Maria Katharine Koldewey ]",
-"[ Johann Christian Vollmer  ] [ Marie Louise Lisette Albersmeier ]",
-"[ Walter Louis Pepler ] [ Mae Behrman ]",
-"[ Ferdinand Heinrich Krueckeberg ] [ Ruth Modesta Vollmer ]",
-"[ John Howard Krueckeberg ] [ Bonnie Carolyn Pepler ]");
 
 // Break the file name into two parts.
 function get_new_bracketed_name($match)
@@ -68,18 +31,13 @@ function get_new_bracketed_name($match)
      
     return implode(' ', $names);
 }
- 
+
+$dir_name_iter = new \CallbackFilterIterator(new DirectoryIterator(dirname(__FILE__)), function(\SplFileInfo $file_info) { return $file_info->isDir(); }
+						);
+             
 $regex = '@\[\s([A-Za-zöäüAÖÄÜß"_ ]+)\]@';
 
-$dir = dirname(__FILE__);
-
-$dir_iter = new DirectoryIterator($dir);
-    
-$dir_only_iter = new \CallbackFilterIterator($dir_iter, function(\SplFileInfo $file_info) {
-                 return $file_info->isDir();
-             });
-             
-foreach($dir_only_iter as $file_info) {
+foreach($dir_names_iter as $file_info) {
 
    $dir_name = $file_info->getFilename();
 
